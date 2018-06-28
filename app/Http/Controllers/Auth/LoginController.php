a4bbb6580ee748c21818f1d1c9a83e8db3a45b16
Chris Cornford <chriscornford12@gmail.com>
2018-06-28 12:42:37 +0100

    Update LoginController to user github as authentication

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,37 +3,45 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Socialite;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
     /**
-     * Where to redirect users after login.
+     * Redirect the user to the GitHub authentication page.
      *
-     * @var string
+     * @return \Illuminate\Http\Response
      */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function redirectToProvider()
     {
-        $this->middleware('guest')->except('logout');
+        return Socialite::driver('github')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $githubUser = Socialite::driver('github')->user();
+        $user = User::where('email','=', $githubUser->getEmail())->first();
+
+        if ($user === null) {
+            $data = [
+                'email' => $githubUser->getEmail(),
+                'name' => $githubUser->getNickName(),
+                'password' => ''
+            ];
+
+            // Create new user based on github user
+            $user = User::firstOrCreate($data);
+        }
+
+        Auth::login($user);
+
+        return redirect()->route('home');
     }
 }
